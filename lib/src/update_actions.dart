4f2dbf0f83feea1da7f59058f402fd0a9e4182a7
Tim <timnew.wti@gmail.com>
2020-11-01 19:04:52 +1100

    Document update_actions

--- lib/src/update_actions.dart
+++ lib/src/update_actions.dart
@@ -1,4 +1,8 @@
-
+/// Contract of synchronous value updater
 typedef T ValueUpdater<T>(T current);
+
+/// Contract of asynchronous value updater
 typedef Future<T> AsyncValueUpdater<T>(T current);
+
+/// Contract of error fixer
 typedef T ErrorFixer<T>(Object error);
